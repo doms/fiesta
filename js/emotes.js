46f2b12ee809ccc444736ef507db7832dcb4e96f
Dominic <notdom@users.noreply.github.com>
2016-10-31 15:25:13 -0500

    this should work...

---
+++ js/emotes.js
@@ -0,0 +1,93 @@
+/*
+ * FFT plugin for dancer.js (specifically for emotes)
+ *
+ * Usage of frequencies being 2px with 1px spacing:
+ *
+ * var dancer = new Dancer('song.ogg'),
+ *     canvas = document.getElementById('fftcanvas');
+ *
+ * dancer.fft(canvas, {width: 2, spacing: 1});
+ */
+
+(function() {
+  Dancer.addPlugin('fft', function(canvasEl, options) {
+    options = options || {};
+    var
+      ctx     = canvasEl.getContext('2d'),
+      h       = canvasEl.height,
+      w       = canvasEl.width,
+      width   = options.width || 1,
+      spacing = options.spacing || 0,
+      count   = options.count || 45;
+      widthMultiplier = 19;
+      heightMultiplier = 1.7;
+      emoteOffsetH = 15;
+      lastUpdate = 0;
+      doUpdateAt = 0;
+
+    ctx.fillStyle = options.fillStyle || "white";
+// ---------------------------------------------------------------------
+    /* Random Emotes Testing */
+
+    // Twitch Emotes
+    var PogChamp = '../img/twitch/pogchamp.png',
+        Kappa = '../img/twitch/kappa.png',
+        ResidentSleeper = '../img/twitch/residentsleeper.png',
+        SeemsGood = '../img/twitch/seemsgood.png';
+
+    // BTTV Emotes
+    var BlacKappa = '../img/BTTV/Blackappa.png',
+        FeelsAmazingMan = '../img/BTTV/FeelsAmazingMan.png',
+        LUL = '../img/BTTV/LUL.png',
+        VisLaud = '../img/BTTV/VisLaud.png';
+
+
+    var twitchEmotes = [PogChamp, Kappa, ResidentSleeper, SeemsGood];
+    var bttvEmotes = [BlacKappa, FeelsAmazingMan, LUL, VisLaud];
+    var fiesta = [PogChamp, Kappa, ResidentSleeper, SeemsGood, BlacKappa, FeelsAmazingMan, LUL, VisLaud];
+
+// ---------------------------------------------------------------------
+
+    var twitchImageObj = new Image();
+    twitchImageObj.src = twitchEmotes[Math.floor(Math.random() * twitchEmotes.length)];
+
+
+    var bttvImageObj = new Image();
+    bttvImageObj.src = bttvEmotes[Math.floor(Math.random() * bttvEmotes.length)];
+
+
+    var fiestaImageObj = new Image();
+    fiestaImageObj.src = fiesta[Math.floor(Math.random() * bttvEmotes.length)];
+
+
+    this.bind('update', function() {
+      var spectrum = this.getSpectrum();
+      ctx.clearRect(0, 0, w, h);
+
+
+      for (var i = 0, l = spectrum.length; i < l && i < count; i++) {
+        if ($('input[name="settings-bars"]').prop('checked')) {
+          ctx.fillRect(i * (spacing + width + widthMultiplier), h, width + 10, -spectrum[i] * h * heightMultiplier);
+        }
+
+        if($('input[name="settings-twitch"]').prop('checked')) {
+          ctx.drawImage(twitchImageObj, i * (spacing + width + widthMultiplier) - 2,  -spectrum[i] * h * heightMultiplier + h - emoteOffsetH, 18, 18);
+        }
+
+        // TODO: show certain emotes depending on height of rectangle.
+        if($('input[name="settings-bttv"]').prop('checked')) {
+          ctx.drawImage(bttvImageObj, i * (spacing + width + widthMultiplier) - 2,  -spectrum[i] * h * heightMultiplier + h - emoteOffsetH, 18, 18);
+        }
+
+        // TODO: show certain emotes depending on height of rectangle.
+        if($('input[name="settings-fiesta"]').prop('checked')) {
+          ctx.drawImage(fiestaImageObj, i * (spacing + width + widthMultiplier) - 2,  -spectrum[i] * h * heightMultiplier + h - emoteOffsetH, 18, 18);
+          fiestaImageObj.src = fiesta[Math.floor(Math.random() * fiesta.length)];
+        }
+      }
+
+    });
+
+    return this;
+  });
+})();
---
+++ js/player.js
@@ -0,0 +1,171 @@
+dancer = new Dancer();
+volume = 0.5;
+ctx = fft.getContext('2d');
+fft = document.getElementById('fft');
+updateSlider = true;
+
+(function () {
+  if(!(window.ActiveXObject) && "ActiveXObject" in window) {
+     $('.drop-message').text('INTERNET EXPLORER IS NOT SUPPORTED!');
+  }
+
+  Dancer.setOptions({
+    flashSWF: '../../lib/soundmanager2.swf',
+    flashJS: '../../lib/soundmanager2.js'
+  });
+
+  // controls the seeker (how long the song has been playing and what point it's at in the song)
+  $('.seeker').slider({
+    start: function(event, ui) {
+      updateSlider = false;
+    },
+    stop: function(event, ui) {
+      updateSlider = true;
+      var seekTo = (ui.value / 100) * dancer.audio.duration;
+
+      if(dancer.audio) {
+        dancer.audio.currentTime = seekTo;
+      }
+    }
+  });
+
+  // controls the volume slider
+  $('.volume').slider({
+    value: 50,
+    slide: function(event, ui) {
+      volume = ui.value / 100;
+
+      if(dancer.audio) {
+        dancer.setVolume(volume);
+      }
+    }
+  });
+
+  //
+  $('.button-stop').click(function() {
+    dancer.pause();
+    setStatusPaused();
+  })
+
+  $('.button-play').click(function() {
+    if(playlist.length) {
+      dancer.play();
+      setStatusPlaying();
+    }
+  })
+
+  $('.button-back').click(function() {
+    var currentTrack = $('.track-playing');
+
+    if(currentTrack.length) {
+      nextTrack = parseInt(currentTrack.attr('data-track')) - 1;
+
+      if(nextTrack < 0) {
+        dancer.audio.currentTime = 0;
+      } else {
+        playTrack(playlist[nextTrack]);
+        $('.track-playing').removeClass('track-playing');
+        $($('#track-list tr')[nextTrack]).addClass('track-playing');
+      }
+
+      setStatusPlaying();
+    }
+  })
+
+  $('.button-forward').click(function() {
+    playNextTrack();
+  })
+
+  setInterval(function() {
+    if(dancer && dancer.audio) {
+      if(updateSlider) {
+        var progress = dancer.audio.currentTime / dancer.audio.duration * 100;
+        $('.seeker').slider('value', progress);
+
+        if(progress === 100) {
+          dancer.pause();
+          playNextTrack();
+        }
+      }
+    }
+  }, 100)
+
+})();
+
+function playTrack(track) {
+  dancer.pause();
+
+  if(dancer.audioAdapter.context.close) {
+    dancer.audioAdapter.context.close();
+  }
+
+  dancer = null;
+  dancer = new Dancer();
+
+  // colors for "fiesta mode"
+  var colors = ['#6441A5', '#1A8BF0', '#7289DA', '#9B0A0A', '#F4FA58',
+                '#00FFFF', '#FFFFFF', '#0000FF', '#00FF00', '#FE2EF7'];
+
+  kick = dancer.createKick({
+    onKick: function () {
+      if ($('input[name="settings-fiesta"]').prop('checked')) {
+        ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+      } else {
+        ctx.fillStyle = '#1A8BF0';
+      }
+    },
+    offKick: function () {
+      if ($('input[name="settings-fiesta"]').prop('checked')) {
+        ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+      } else {
+        ctx.fillStyle = '#1A8BF0';
+      }
+    }
+  }).on();
+
+  audio = new Audio();
+  audio.src = track.blob
+
+  dancer.fft( fft, { fillStyle: '#666' });
+  dancer.load(audio);
+  dancer.setVolume(volume);
+  dancer.play();
+
+  setStatusPlaying();
+}
+
+function setStatusPlaying() {
+  if(!$('body').hasClass('is-playing')) {
+    $('body').addClass('is-playing');
+  }
+
+  $('.drop-message').hide(); // hides "drag & drop" message
+}
+
+function setStatusPaused() {
+  if($('body').hasClass('is-playing')) {
+    $('body').removeClass('is-playing');
+  }
+}
+
+function playNextTrack() {
+  var currentTrack = $('.track-playing');
+
+  if(currentTrack.length) {
+    nextTrack = parseInt(currentTrack.attr('data-track')) + 1;
+
+    console.log(playlist[nextTrack]);
+
+    if(playlist[nextTrack]) {
+      playTrack(playlist[nextTrack]);
+      $('.track-playing').removeClass('track-playing');
+      $($('#track-list tr')[nextTrack]).addClass('track-playing');
+    } else {
+      playTrack(playlist[0]);
+      $('.track-playing').removeClass('track-playing');
+      $($('#track-list tr')[0]).addClass('track-playing');
+    }
+
+    setStatusPlaying();
+  }
+}
