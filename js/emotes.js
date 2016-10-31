04f33a4bf3759bf5ae86ab489b80abbcce56e718
Dominic <notdom@users.noreply.github.com>
2016-10-31 16:49:57 -0500

    bug fixes

--- js/emotes.js
+++ js/emotes.js
@@ -27,6 +27,24 @@
 
     ctx.fillStyle = options.fillStyle || "white";
 // ---------------------------------------------------------------------
+    /* Random Emotes Testing */
+
+    // Twitch Emotes
+    var PogChamp = '/img/twitch/pogchamp.png',
+        Kappa = '/img/twitch/kappa.png',
+        ResidentSleeper = '/img/twitch/residentsleeper.png',
+        SeemsGood = '/img/twitch/seemsgood.png';
+
+    var emotes = [PogChamp, Kappa, ResidentSleeper, SeemsGood];
+
+// ---------------------------------------------------------------------
+
+    var emotesObj = new Image();
+    emotesObj.src = emotes[Math.floor(Math.random() * emotes.length)];
+
+    var fiestaObj = new Image();
+    fiestaObj.src = emotes[Math.floor(Math.random() * emotes.length)];
+
 
     this.bind('update', function() {
       var spectrum = this.getSpectrum();
@@ -36,6 +54,16 @@
       for (var i = 0, l = spectrum.length; i < l && i < count; i++) {
         if ($('input[name="settings-bars"]').prop('checked')) {
           ctx.fillRect(i * (spacing + width + widthMultiplier), h, width + 10, -spectrum[i] * h * heightMultiplier);
+        }
+
+        if($('input[name="settings-twitch"]').prop('checked')) {
+          ctx.drawImage(emotes, i * (spacing + width + widthMultiplier) - 2,  -spectrum[i] * h * heightMultiplier + h - emoteOffsetH, 18, 18);
+        }
+
+        // TODO: show certain emotes depending on height of rectangle.
+        if($('input[name="settings-fiesta"]').prop('checked')) {
+          ctx.drawImage(fiestaObj, i * (spacing + width + widthMultiplier) - 2,  -spectrum[i] * h * heightMultiplier + h - emoteOffsetH, 18, 18);
+          fiestaObj.src = emotes[Math.floor(Math.random() * emotes.length)];
         }
       }
 
